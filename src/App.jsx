dad37c45b32d238d8646548bf895ae0abfcced81
Facundo Herrera <facundhfed@gmail.com>
2022-04-22 09:04:59 +0200

    Styled components creados

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,60 @@
 import { useState } from 'react'
+import styled from '@emotion/styled';
+
+import ImagenCripto from './img/imagen-criptos.png'
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px){
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+
+  }
+`
+ const Imagen = styled.img`
+   max-width: 400px;
+   width: 80%;
+   margin: 100px auto 0 auto;
+   display: block;
+ `
+
+ const Heading = styled.h1`
+  font-family:'Quicksand', sans-serif;
+  text-transform: uppercase;
+  text-align: center;
+  margin-top: 120px;
+  margin-bottom: 50px;
+  font-weight: 700;
+  font-size: 35px;
+  ::after{
+      content: '';
+      width: 100px;
+      height: 6px;
+      background-color:#40D3DE ;
+      display: block;
+      margin: 10px auto 0 auto ;
+  }
+ `;
 
 
 function App() {
   
-
   return (
-    <h1>Desde App</h1>
+    <Container>
+      <Imagen
+        src={ImagenCripto}
+        alt='imagen criptomonedas'
+      />
+
+      <div>
+        <Heading>Cotizador Criptomonedas React </Heading>
+      </div>
+      
+
+    </Container>
   )
 }
 
